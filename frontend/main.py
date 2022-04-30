589843ade055a38ba1cf8119512c44d70da9d732
Nasri Islam ,Lee <nasri@thinknet.co.th>
2022-05-01 06:27:33 +0700

    [ADD] if condition when frame is not None

--- frontend/main.py
+++ frontend/main.py
@@ -14,7 +14,7 @@ tfile = tempfile.NamedTemporaryFile(delete=False)
 if video_upload is not None:
     tfile.write(video_upload.read())
 
-video = cv2.VideoCapture(tfile.name)
+video = cv2.VideoCapture(tfile.name, cv2.CAP_FFMPEG)
 
 col1, col2= st.columns([1, 4])
 
@@ -26,10 +26,15 @@ with col1:
 with col2:
     FRAME_WINDOW = st.image([])
 
-while genre == "Webcam":
+while genre == "Webcam" and camera.isOpened():
     _, frame = camera.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    FRAME_WINDOW.image(frame)
+    if frame is not None:
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        FRAME_WINDOW.image(frame)
+    else:
+        break
+
+camera.release() 
 
 
 while genre == "Video" and video.isOpened():
@@ -37,7 +42,11 @@ while genre == "Video" and video.isOpened():
     if frame is not None:
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         FRAME_WINDOW.image(frame)
+    else:
+        break
+
+video.release()  
     
 col1, col2, col3 = st.columns(3)
-col2.metric("2D", "Live", "-spoof")
+col2.metric("2D", "Spoof", "-spoof")
 col3.metric("3D", "Live", "+live")
