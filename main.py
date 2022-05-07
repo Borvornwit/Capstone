67952eccee15725c7489edf51e8a6fe512116e49
Terash <terash2000@gmail.com>
2022-05-07 23:01:49 +0700

    add preprocess thread

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import streamlit as st
 import tempfile
 import sys, os
 import threading
+import numpy as np
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/Api code 2D')
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/Api code 3D')
@@ -14,9 +15,9 @@ from faceLandmark import faceLandmark
 #from faceAntiSpoof2D_old import faceAntiSpoof2D_old
 
 _faceAntiSpoof2D = faceAntiSpoof2D(modelFile = 'Api code 2D/model/best')
+#_faceAntiSpoof2D = faceAntiSpoof2D_old(modelFile = 'Api code 2D/model/old')
 _faceAntiSpoof3D = FAS3D(modelFile='Api code 3D/Tranrppg10sec new model_drop_5.pt')
 _faceLandmarks = faceLandmark('faceDetetorAndAlignment/faceLandmark.py')
-#_faceAntiSpoof2D = faceAntiSpoof2D_old(modelFile = 'Api code 2D/model/old')
 
 st.title("Face-Anti spoofing - DEMO")
 
@@ -61,24 +62,64 @@ def updateResult():
         col2.metric("2D", result_2d_str, str(score_2d))
         col3.metric("3D", result_3d_str)
 
+def preprocess(frame):
+    global alignedImage, faceBoxes, faceLandmarks
+    alignedImage, faceBoxes = _faceAntiSpoof2D.cropImage(frame)
+    faceLandmarks = _faceLandmarks.extractLandmark(frame, faceBoxes)
+
 def predict2d(frame):
     global result_2d, score_2d
     result_2d, score_2d = _faceAntiSpoof2D.detectAfterPreprocess(frame)
 
 def predict3d(final_mstmap_face,final_mstmap_bg):
+    global result_3d
     if final_mstmap_face is None: 
         result_3d = None
         return
     result_3d = _faceAntiSpoof3D.predict(final_mstmap_face,final_mstmap_bg)
 
+def faceAntiSpoof(frame):
+    global thread_preprocess, thread_2d, thread_3d
+    global frame_count
+
+    # preprocess
+    if not thread_preprocess.is_alive():
+        thread_preprocess = threading.Thread(target=preprocess, args=(frame,))
+        thread_preprocess.start()
+
+    # if face not detected
+    if len(faceBoxes) == 0:
+        frame_count = 0
+        return
+
+    # 2DFAS
+    if not thread_2d.is_alive():
+        thread_2d = threading.Thread(target=predict2d, args=(alignedImage,))
+        thread_2d.start()
+
+    # 3DFAS
+    x1,y1,x2,y2,_ = faceBoxes[0].astype(np.int32)
+    bgLandmarks = [x1,y1,x2,y2,w,h]
+    final_mstmap_face,final_mstmap_bg = _faceAntiSpoof3D.genFromSeq(frame,facelandmarks,bgLandmarks,frame_count,cam_id=0) 
+    frame_count += 1
+
+    if not thread_3d.is_alive():
+        thread_3d = threading.Thread(target=predict3d, args=(frame,final_mstmap_face,final_mstmap_bg,))
+        thread_3d.start()
+
+thread_preprocess = threading.Thread(target=preprocess)
+thread_2d = threading.Thread(target=predict2d)
+thread_3d = threading.Thread(target=predict3d)
+
+alignedImage = None
+faceBoxes = []
+faceLandmarks = None
 
 result_2d = None
 score_2d = 0.0
+
 result_3d = None
-thread_2d = threading.Thread(target=predict2d)
-thread_3d = threading.Thread(target=predict3d)
-final_mstmap_face = None
-final_mstmap_bg = None
+frame_count = 0
 
 placeholder = st.empty()
 
@@ -89,30 +130,8 @@ while genre == "Webcam" and camera.isOpened():
         h , w,_ = frame.shape
         FRAME_WINDOW.image(frame)
 
+        faceAntiSpoof(frame)
         updateResult()
-
-        alignedImage, faceBoxes = _faceAntiSpoof2D.cropImage(frame)
-        faceLandmarks = _faceLandmarks.extractLandmark(frame, faceBoxes)
-
-        # if face not detected
-        if len(faceBoxes) == 0:
-            frame_count = 0
-            continue
-
-        if not thread_2d.is_alive():
-            thread_2d = threading.Thread(target=predict2d, args=(alignedImage,))
-            thread_2d.start()
-
-        # 3DFAS
-        x1,y1,x2,y2,_ = faceBoxes[0].astype(np.int32)
-        bgLandmarks = [x1,y1,x2,y2,w,h]
-        final_mstmap_face,final_mstmap_bg = _faceAntiSpoof3D.genFromSeq(frame,facelandmarks,bgLandmarks,frame_count,cam_id=0) 
-        frame_count += 1
-
-        if not thread_3d.is_alive():
-            thread_3d = threading.Thread(target=predict3d, args=(frame,final_mstmap_face,final_mstmap_bg,))
-            thread_3d.start()
-
     else:
         break
 
@@ -125,17 +144,8 @@ while genre == "Video" and video.isOpened():
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         FRAME_WINDOW.image(frame)
 
+        faceAntiSpoof(frame)
         updateResult()
-
-        alignedImage, faceBoxes = _faceAntiSpoof2D.cropImage(frame)
-
-        # if face not detected
-        if len(faceBoxes) == 0:
-            continue
-
-        if not thread_2d.is_alive():
-            thread_2d = threading.Thread(target=predict2d, args=(alignedImage,))
-            thread_2d.start()
     else:
         break
 
